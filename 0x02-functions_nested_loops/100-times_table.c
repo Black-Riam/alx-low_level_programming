8b06e73108075daae873afb2ed417678c58c4839
Blacklov <mafadusei@gmail.com>
2022-04-13 16:01:58 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,8 +17,8 @@ void print_times_table(int n)
 
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 
 				prod = num * mult;
 
