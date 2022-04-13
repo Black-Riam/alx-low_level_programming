a97b2d5468d89be41f6a24970c7fcd1bbe384c6a
Blacklov <mafadusei@gmail.com>
2022-04-12 18:43:24 -0700

    signs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,12 @@
 /**
  * _abs - return abs value
  * @n : number to check
- * Return: 0 or 1
+ * Return:0 or 1
  */
 
 int _abs(int n)
 {
-	return (n >= 0 ? n : * -1);
+
+return (n >= 0 ? n : n * -1);
+
 }
