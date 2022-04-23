3113a54b16ff786d2335a99cd143d04a2fd8b0c6
Blacklov <mafadusei@gmail.com>
2022-04-22 18:29:42 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,25 +10,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, k = 0;
-
-	while (n > k)
-	{
-		if (src[k] == '\0')
-		{
-			for (; k < n; k++)
-			{
-				dest[i] = '\0';
-					i++;
-			}
-		}
-		else
-		{
-			dest[i] = src[k];
-			k++;
-			i++;
-		}
-	}
-
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	
 	return (dest);
 }
