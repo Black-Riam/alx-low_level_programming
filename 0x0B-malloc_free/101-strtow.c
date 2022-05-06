26f14e9d619a467de7b680ac999daf0c96d65cfb
Blacklov <mafadusei@gmail.com>
2022-05-05 18:07:57 -0700

    matrix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,6 +12,7 @@ int count_word(char *s)
 	int flag, c, w;
 	flag = 0;
 	w = 0;
+
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
@@ -34,8 +35,7 @@ int count_word(char *s)
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
-	int i, k = 0, len = 0, words, c = 0, start, end;
-	
+	int i, k = 0, len = 0, words, c = 0, start, end;	
 	while (*(str + len))
 		len++;
 	words = count_word(str);
