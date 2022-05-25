3c3d5755f8def6b4832bcacd965d4f02ccbbe07b
Blacklov <mafadusei@gmail.com>
2022-05-25 10:10:53 -0700

    holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,21 @@
-SECTION .data
-msg: db "Hello, Holberton", 0
-fmt: db %s", 10, 0
-
-SECTION .text
 extern printf
-global main
-main:
-mov esi, msg
-mov edi, fmt
-mov eax, 0
-call printf
 
-mov eax, 0
-ret
+section .text
+   global main
+
+main:
+   push rbp
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
+
+   pop rbp
+
+   mov rax,0
+   ret
+
+section .data
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
