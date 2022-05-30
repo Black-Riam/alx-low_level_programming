720e61022c3509cae8f69c92fe4104beb806c630
Blacklov <mafadusei@gmail.com>
2022-05-30 15:16:08 -0700

    main file

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,20 @@
+File: _putchar
+
+#include <unistd.h>
+
+/**
+ * Auth: Blacklov 
+ */
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
